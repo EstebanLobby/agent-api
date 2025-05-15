b29fcd9c9d2852eb67653cb497f1d009d858e7f1
FabianaLobby <fabiana@lobby-digital.com>
2025-05-15 16:00:09 -0300

    T36: fead: delete unnecesary imports

--- public/src/components/dashboard/layout/main-nav.tsx
+++ public/src/components/dashboard/layout/main-nav.tsx
@@ -2,12 +2,9 @@
 
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Badge from '@mui/material/Badge';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
-import Tooltip from '@mui/material/Tooltip';
-import { Bell as BellIcon } from '@phosphor-icons/react/dist/ssr/Bell';
 import { List as ListIcon } from '@phosphor-icons/react/dist/ssr/List';
 
 import { usePopover } from '@/hooks/use-popover';
@@ -19,7 +16,6 @@ import { UserPopover } from './user-popover';
 export function MainNav(): React.JSX.Element {
   const [openNav, setOpenNav] = React.useState<boolean>(false);
   const { user } = useUser();
-  console.log(user);
   const userPopover = usePopover<HTMLDivElement>();
   return (
     <React.Fragment>
